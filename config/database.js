d5ed2badf70a5f0e0a357fd81328bd4b3bd44d45
Gervasio Benardo Chavane <gervasiochavane798@gmail.com>
2025-02-26 23:09:17 +0200

    Fix missing sqlite3 module

--- config/database.js
+++ config/database.js
@@ -1,24 +1,17 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_CLIENT', 'mysql'); // Garantir que o cliente seja mysql
 
   const connections = {
     mysql: {
       connection: {
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'mysql.railway.internal'), // Usar o host do Railway
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
+        database: env('DATABASE_NAME', 'railway'), // Banco de dados do Railway
+        user: env('DATABASE_USERNAME', 'root'), // Usuário do Railway
+        password: env('DATABASE_PASSWORD', 'LszqtvubJDoXRBPMMblSTZxVRCYTuYPP'), // Senha do Railway
+        ssl: env.bool('DATABASE_SSL', false), // Não SSL no MySQL no Railway
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
